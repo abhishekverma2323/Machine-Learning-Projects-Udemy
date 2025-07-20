bb6f8edbba6a3ac83414ecb6d03d13fbf352c947
Abhishek Verma <abhishekverma3036@gmail.com>
2025-07-20 20:00:54 +0530

    Day 2: Dealing with Categorical data OneHotEncoder
    learning how to deal with categorical data and how to apply OneHotEncoder technique to divide the categories into the Number

--- Preprocessing the data.py
+++ Preprocessing the data.py
@@ -22,3 +22,12 @@ imputer.fit(x[:, 1:3])
 x[:,1:3]=imputer.transform(x[:,1:3])
 
 print(x)
+print()
+
+#dealing with the categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])], remainder='passthrough')
+x=np.array(ct.fit_transform(x))
+print(x)
